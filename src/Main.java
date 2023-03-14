3fb5963f109909bbbb19ce9323a21014337b1c38
YewandeOyewole <wandeoyewo@gmail.com>
2023-03-14 14:25:09 +0100

    Added TestCodeCamp 'Task 14'

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world.");
+        System.out.println("This is Yewande");
+        System.out.println("This is my first Java project");
+        System.out.println("Im a member of team postman cohurt 13");
+        System.out.println("its as not been an easy ride so far");
+        System.out.println("it as been a challenging one ");
+        System.out.println("I look forward to learning more");
+        System.out.println("I look forward to been a good software tester");
+        System.out.println("Thank you");
+    }
+}
